e558469bbdf3eba444190cd4363d928efad7f56c
NikhilGathal <nikhilgathal@gmail.com>
2023-10-16 08:24:09 +0530

    JavaScript Assignment

---
+++ JavaScript-1.js
@@ -0,0 +1,21 @@
+function reverseWords(sentence) {
+
+    const words = sentence.split(' ');
+  
+
+    const reversedWords = words.map(word => {
+
+      const reversedWord = word.split('').reverse().join('');
+      return reversedWord;
+    });
+  
+
+    const reversedSentence = reversedWords.join(' ');
+  
+    return reversedSentence;
+  }
+  
+  const inputSentence = "This is a sunny day";
+  const reversedSentence = reverseWords(inputSentence);
+  console.log(reversedSentence);
+  
---
+++ JavaScript-2.js
@@ -0,0 +1,10 @@
+
+
+
+const array = [5, 2, 9, 1, 5, 6];
+
+array.sort(function(a, b) {
+  return b - a;
+});
+
+console.log(array); 
